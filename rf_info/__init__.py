491d088c2f429f29eeec08910abaa6e6705d70d1
cosmicc <ianperry99@gmail.com>
2020-01-16 22:42:13 -0500

    Bump version: 0.5.1 → 0.5.2

--- rf_info/__init__.py
+++ rf_info/__init__.py
@@ -2,7 +2,7 @@ from .rf_info import Frequency
 
 __author__ = """Ian Perry"""
 __email__ = 'ianperry99@gmail.com'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __license__ = 'MIT'
 __progname__ = 'rf-info'
 __description__ = "Radio Frequency Information"
