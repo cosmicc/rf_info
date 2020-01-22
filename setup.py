ce8aa4e951187eb0ad1a1939f2056eb6381502e0
cosmicc <ianperry99@gmail.com>
2020-01-22 00:20:45 -0500

    Bump version: 0.6.3 → 0.7.0

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cosmicc/rf_info',
-    version='0.6.3',
+    version='0.7.0',
     zip_safe=False,
 )
